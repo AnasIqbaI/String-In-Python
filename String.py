d5a0ca877a7942ee5b08979c605574df8b03731c
AnasIqbaI <arafdarky@gmail.com>
2025-07-13 23:25:19 +0600

    String in python

---
+++ String.py
@@ -0,0 +1,15 @@
+first_name = "Codingal"
+last_name = "Academy"
+full_name = first_name+last_name
+example = "HAA"*5
+
+print("Full Name:", full_name)
+print("Last Name:", last_name)
+print("Full Name :", first_name+last_name)
+print("string multiplied by 5:", example)
+
+word = "Coding"
+print("Length of string:", len(word))
+print("First character:", word[0])
+print("Last character:", word[5])
+print("String Sliced:", word[0:3]) 
